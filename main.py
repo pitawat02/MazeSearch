e040ddf9aedf9978828dee808977c0d236d39f64
[pitawat02] <[pitawat02@gmail.com]>
2020-09-01 19:02:18 +0700

    startpoint
    just import

---
+++ main.py
@@ -0,0 +1,16 @@
+import search
+from graph import Graph
+
+
+if __name__ == "__main__":
+    # Setting graph we initiated to search class...
+
+    graph = Graph()
+    search.graph = graph
+
+    search.depth_first_search()
+    search.breath_first_search()
+    search.iterative_deepening_search()
+    search.uniform_cost_search()
+    search.greedy_best_first_search()
+    search.a_star_search()
